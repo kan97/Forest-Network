450c27701399b2f1b8a9ea8f196be9570c885f43
Tieu Tri Kiet <kiettt@evolableasia.vn>
2018-12-04 17:02:49 +0700

    Update your comment and something

--- src/components/post/post.jsx
+++ src/components/post/post.jsx
@@ -6,10 +6,10 @@ import Comment from '../comment/comment';
 class Post extends Component {
     constructor(props) {
         super(props);
-        this.state = { 
+        this.state = {
             isExpandComment: false,
             isLiked: false
-         }
+        }
     }
 
     componentDidMount = () => {
@@ -42,46 +42,37 @@ class Post extends Component {
         if (this.props.postImage) {
             content.push(<img src={this.props.postImage} alt={this.props.postImage} className="post-image" />);
         }
-        
+
         if (content.length === 0) {
-            content.push(<div/>)
+            content.push(<div />)
         }
 
         return content;
     };
 
-    getComments = () => {
-        let comments = [];
-        comments.push(
-            <Comment
-                ownerAvatar="https://www.muralswallpaper.com/app/uploads/blue-grunge-ombre-design-square-1-400x400.jpg"
-                ownerName="Tiêu Trí Kiệt"
-                comment="Good!"
-                commentTime="03/12/2018"
-            />
-        );
-
-        comments.push(
-            <Comment
-                ownerAvatar="https://www.muralswallpaper.com/app/uploads/Aztec-Diamonds-Pattern-Wallpaper-Mural-Square-400x400.jpg"
-                ownerName="Kiệt Tiêu"
-                comment="This is perfect comment !?"
-                commentTime="02/12/2018"
-            />
-        );
-
-        comments.push(
-            <Comment
-                ownerAvatar="http://yatheatre.com/wp-content/uploads/2018/08/Replaceable-The-Office-Wallpaper-Hd-005-art-deco-4876776952521969087-wallsneedlove-.jpg"
-                ownerName="Tiêu"
-                comment="That right!"
-                commentTime="01/12/2018"
-            />
-        );
-
+    showCommentText = () => {
         return (
-            <div className="row post-comment">
-                {comments}
+            <div>
+                <div className="post-horizal-line" />
+                <div className="row post-write-comment">
+                    <div className="col-sm-10">
+                        <input type="text" class="form-control post-your-comment" id="comment" placeholder="Your comment" />
+                    </div>
+
+                    <div className="col-sm-2">
+                        <button type="button" className="btn btn-primary">Send</button>
+                    </div>
+                </div>
+            </div>
+
+        );
+    }
+
+    showShareTimes = () => {
+        return (
+            <div className="col-sm-2 text-left post-react-button">
+                <span class="glyphicon glyphicon-send post-react"></span>
+                {this.props.postShare}
             </div>
         );
     }
@@ -115,18 +106,53 @@ class Post extends Component {
                         <span class="glyphicon glyphicon-align-left post-react"></span>
                         {this.props.postComment}
                     </div>
-                    <div className="col-sm-2 text-left post-react-button">
-                        <span class="glyphicon glyphicon-send post-react"></span>
-                        {this.props.postShare}
-                    </div>
+
+                    {this.props.isYourPost ? <div className="col-sm-2 text-left post-react-button" /> : this.showShareTimes()}
                     <div className="col-sm-6"></div>
                 </div>
 
                 {this.state.isExpandComment ? <div className="post-horizal-line" /> : <div />}
-                {this.state.isExpandComment ? this.getComments() : <div />}
+                {this.state.isExpandComment ? getComments() : <div />}
+                {this.state.isExpandComment ? this.showCommentText() : <div />}
             </div>
         );
     }
 }
 
 export default Post;
+
+const getComments = () => {
+    let comments = [];
+    comments.push(
+        <Comment
+            ownerAvatar="https://www.muralswallpaper.com/app/uploads/blue-grunge-ombre-design-square-1-400x400.jpg"
+            ownerName="Tiêu Trí Kiệt"
+            comment="Good!"
+            commentTime="03/12/2018"
+        />
+    );
+
+    comments.push(
+        <Comment
+            ownerAvatar="https://www.muralswallpaper.com/app/uploads/Aztec-Diamonds-Pattern-Wallpaper-Mural-Square-400x400.jpg"
+            ownerName="Kiệt Tiêu"
+            comment="This is perfect comment !?"
+            commentTime="02/12/2018"
+        />
+    );
+
+    comments.push(
+        <Comment
+            ownerAvatar="http://yatheatre.com/wp-content/uploads/2018/08/Replaceable-The-Office-Wallpaper-Hd-005-art-deco-4876776952521969087-wallsneedlove-.jpg"
+            ownerName="Tiêu"
+            comment="That right!"
+            commentTime="01/12/2018"
+        />
+    );
+
+    return (
+        <div className="row post-comment">
+            {comments}
+        </div>
+    );
+}
