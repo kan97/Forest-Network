778f2ca1574765897a6dabd758f0ff35883868c8
Tieu Tri Kiet <kiettt@evolableasia.vn>
2018-12-04 09:07:43 +0700

    Add follower and following list

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import './App.css';
 
 import Post from './components/post/post';
 import UserInfo from './components/userInfo/userInfo';
+import FollowerList from './components/follower/followerList';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        {getPostExample()}
+        {getList()}
       </div>
     );
   }
@@ -36,4 +37,100 @@ const getUserInfoExample = () => {
   return (<UserInfo 
     userAvatar="https://www.muralswallpaper.com/app/uploads/aquamarine-patterned-ombre-wall-mural-square-400x400.jpg"
   />);
+}
+
+const getList = () => {
+  return (
+    <FollowerList list={getFollowingList()} />
+  );
+}
+
+const getFollowerList = () => {
+  let list = [];
+  list.push(
+    {
+      avatar:"https://instagram.fsgn2-3.fna.fbcdn.net/vp/47ec7e42667162b88ee6edad54156584/5CA352F1/t51.2885-19/s150x150/44377321_281267495857242_1536691637256716288_n.jpg",
+      name:"KTT",
+      isFollowing:true
+    }
+  )
+
+  list.push(
+    {
+      avatar:"https://beebom-redkapmedia.netdna-ssl.com/wp-content/uploads/2016/01/Reverse-Image-Search-Engines-Apps-And-Its-Uses-2016.jpg",
+      name:"Miao Miao",
+      isFollowing:false
+    }
+  )
+
+  list.push(
+    {
+      avatar:"https://www.wonderplugin.com/videos/demo-image0.jpg",
+      name:"Sight",
+      isFollowing:true
+    }
+  )
+
+  list.push(
+    {
+      avatar:"https://images.pexels.com/photos/658687/pexels-photo-658687.jpeg?auto=compress&cs=tinysrgb&h=350",
+      name:"Rosike",
+      isFollowing:true
+    }
+  )
+
+  list.push(
+    {
+      avatar:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQuGU91BKoHbBa9bry8Go-TTW9t263vcG9aqzaIwrBfNtWanOeq9Q",
+      name:"Sunflower",
+      isFollowing:false
+    }
+  )
+
+  return list;
+}
+
+const getFollowingList = () => {
+  let list = [];
+  list.push(
+    {
+      avatar:"https://i.pinimg.com/originals/6a/bc/b6/6abcb6df12595ae4eeb390ebf2610cba.png",
+      name:"Eevee",
+      isFollowing:true
+    }
+  )
+
+  list.push(
+    {
+      avatar:"https://cdn.gamerant.com/wp-content/uploads/new-pokemon-go-additions-datamine.jpg.optimal.jpg",
+      name:"Jirachi",
+      isFollowing:true
+    }
+  )
+
+  list.push(
+    {
+      avatar:"https://pokemongohub.net/wp-content/uploads/2018/03/mew-pokemon-go.jpg",
+      name:"Mew mew",
+      isFollowing:true
+    }
+  )
+
+  list.push(
+    {
+      avatar:"https://d2skuhm0vrry40.cloudfront.net/2018/articles/2018-03-28-11-25/Mareep.jpg/EG11/resize/300x-1/quality/75/format/jpg",
+      name:"Mereep beeee",
+      isFollowing:true
+    }
+  )
+
+  list.push(
+    {
+      avatar:"https://pokemongolive.com/img/posts/unibailsafarizone.jpg",
+      name:"GO safari",
+      isFollowing:true
+    }
+  )
+
+  return list;
 }
---
+++ src/components/follower/follower.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './follower.css';
+
+
+class Follower extends Component {
+
+    render() {
+        return (
+            <div className="row follow-container">
+                <div className="col-sm-1">
+                    <img className="follow-avatar" src={this.props.avatar} alt={this.props.avatar} />
+                </div>
+                <div className="col-sm-8 follow-name">
+                    {this.props.name}
+                </div>
+                <div className="col-sm-3">
+                    {this.props.isFollowing ? <button type="button" className="btn btn-default follow-button">Following</button> : <button type="button" className="btn btn-primary follow-button">Follow</button>}
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Follower;
