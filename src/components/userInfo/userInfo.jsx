2bed5961a66a5f191d20d07576eac1362ad68104
Tieu Tri Kiet <kiettt@evolableasia.vn>
2018-12-04 16:28:37 +0700

    Fix profile editor

--- src/components/userInfo/userInfo.jsx
+++ src/components/userInfo/userInfo.jsx
@@ -17,27 +17,37 @@ class UserInfo extends Component {
 
                 <div className="row info-form">
                     <div className="col-sm-3 info-label">Name</div>
-                    <div className="col-sm-9"><input type="text" class="form-control" id="usr" /></div>
+                    <div className="col-sm-9"><input type="text" class="form-control" id="name" /></div>
                 </div>
 
                 <div className="row info-form">
                     <div className="col-sm-3 info-label">Gender</div>
-                    <div className="col-sm-9"><input type="text" class="form-control" id="usr" /></div>
+                    <div className="col-sm-9">
+                        <select class="form-control" id="gender">
+                            <option>Male</option>
+                            <option>Female</option>
+                        </select>
+                    </div>
                 </div>
 
                 <div className="row info-form">
                     <div className="col-sm-3 info-label">Email</div>
-                    <div className="col-sm-9"><input type="text" class="form-control" id="usr" /></div>
+                    <div className="col-sm-9"><input type="email" class="form-control" id="email" /></div>
                 </div>
 
                 <div className="row info-form">
                     <div className="col-sm-3 info-label">Phone number</div>
-                    <div className="col-sm-9"><input type="text" class="form-control" id="usr" /></div>
+                    <div className="col-sm-9"><input type="text" class="form-control" id="phone" /></div>
                 </div>
 
                 <div className="row info-form">
                     <div className="col-sm-3 info-label">Address</div>
-                    <div className="col-sm-9"><input type="text" class="form-control" id="usr" /></div>
+                    <div className="col-sm-9"><input type="text" class="form-control" id="address" /></div>
+                </div>
+
+                <div className="row info-form">
+                    <div className="col-sm-3 info-label">Bio</div>
+                    <div className="col-sm-9"><textarea class="form-control" rows="5" id="bio" /></div>
                 </div>
             </div>
         );
