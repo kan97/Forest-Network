429981ab50a181225911cc750bd54d854880c102
lamlh@evolableasia.vn <lamlh@evolableasia.vn>
2018-12-24 17:00:09 +0700

    update Login LogOut

---
+++ src/helper/UTILS.js
@@ -0,0 +1,28 @@
+var Parse = require('./parseSDK');
+
+const UTILS = {};
+
+UTILS.callAPI = function (apiName, paramsData) {
+    return Parse.Cloud.run(apiName, paramsData);
+}
+
+UTILS.ParseLogIn = function (key) {
+    return Parse.User.logIn(key, '1').then((user) => {
+        const userJSON = user.toJSON();
+
+        return Promise.resolve(userJSON);
+    }).catch((error) => {
+        console.log(error);
+        return Promise.reject(error);
+    });
+}
+
+UTILS.ParseLogOut = function () {
+    return Parse.User.logOut();
+}
+
+UTILS.ParseUserBecome = function(ssTk) {
+    return Parse.User.become(ssTk);
+}
+
+module.exports = UTILS;
